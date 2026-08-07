2d54e1e794601e12d9b549b13814db1eb827c897
marcelinfreefire153-arch <marcelinfreefire153@gmail.com>
2026-08-06 23:16:45 -0300

    Conectar endpoint de chamadas Daily

--- supabase-config.js
+++ supabase-config.js
@@ -3,6 +3,6 @@ window.POSSIVEL_SUPABASE = {
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53eW1zaXV3aXF5dnZtZGFnemVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1NDk1MTEsImV4cCI6MjEwMTEyNTUxMX0.PRlMlBncRxJDcbYtteGkQM86vcDlTSyPexsQB402I6Y',
   checkoutFunctionUrl: 'https://nwymsiuwiqyvvmdagzeg.supabase.co/functions/v1/create-subscription-checkout',
   paymentFunctionUrl: 'https://nwymsiuwiqyvvmdagzeg.supabase.co/functions/v1/create-payment-checkout',
-  callFunctionUrl: '',
+  callFunctionUrl: 'https://nwymsiuwiqyvvmdagzeg.supabase.co/functions/v1/create-call-room',
   proPriceLabel: 'Assinatura mensal'
 };
